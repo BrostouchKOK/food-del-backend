ea3d4891b034d803aa4b8438a9452c0dd4d3912c
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-06-02 18:19:43 +0700

    create shop cart functionality

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { conectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import "dotenv/config.js";
+import cartRouter from "./routes/cartRoute.js";
 
 // app config
 const app = express();
@@ -20,6 +21,7 @@ conectDB();
 app.use("/api/food", foodRouter);
 app.use("/images",express.static('uploads'));
 app.use("/api/user",userRouter);
+app.use("/api/cart",cartRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working Let'go");
